8f974915de7f936c3f344285de670d787e1439bc
Aleksandar Djordjevic <adjordjevic@firstbeatmedia.com>
2016-10-26 10:37:31 +0200

    Initial commit
    Website

---
+++ js/main.js
@@ -0,0 +1,23 @@
+	    $(function(){
+	    	$(".dropdown").hover(            
+	            function() {
+	                $('.dropdown-menu', this).stop( true, true ).fadeIn("fast");
+	                $(this).toggleClass('open');
+	                $('b', this).toggleClass("caret caret-up");                
+	            },
+	            function() {
+	                $('.dropdown-menu', this).stop( true, true ).fadeOut("fast");
+	                $(this).toggleClass('open');
+	                $('b', this).toggleClass("caret caret-up");                
+	            });
+	    });
+
+	    $(function(){
+      		$(".type-element").typed({
+        		strings: ["Developer", "Designer", "Project Manager"],
+        		typeSpeed: 200,
+        		loop: true,
+	            loopCount: false,
+	            showCursor: true
+      		});
+ 		});
